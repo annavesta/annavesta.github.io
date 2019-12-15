47bb737533db55ec261b5abd933664ccc4b8ae83
annavesta <53700680+annavesta@users.noreply.github.com>
2019-12-15 21:22:42 +0300

    базы данных

--- index/bazadannix/static/js/main.js
+++ index/bazadannix/static/js/main.js
@@ -1,18 +1,3 @@
 $(document).ready(function () {
-    var tab = $('.tab');
-    tab.click(function(){
-    	console.log('клик по табу');
-    	var target = $(this).attr('data-target');
-    	console.log('эта вкладка ведет к содержимому'+ target);
-    	$('.tab-content').hide();
-    	$('.'+target).show();
-    });
-    var work = $('.work');
-    work.click(function(){
-    	console.log('клик по табу');
-    	var target = $(this).attr('data-target');
-    	console.log('эта вкладка ведет к содержимому'+ target);
-    	$('.work-content').hide();
-    	$('.'+target).show();
-    });
+    // js code
 });
