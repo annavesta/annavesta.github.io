dbb77b5ac9cef68b3a20075210672502c1153f39
annavesta <53700680+annavesta@users.noreply.github.com>
2020-05-07 14:11:11 +0300

    Add files via upload

---
+++ index/ds/static/js/main.js
@@ -0,0 +1,70 @@
+"use strict";
+
+$(document).ready(function () {
+  $('.slick-vertical').slick({
+    vertical: true,
+    verticalSwiping: true,
+    slidesToShow: 2,
+    prevArrow: '<img src="static/images/top.png">',
+    nextArrow: '<img src="static/images/bottom.png">'
+  });
+  $('.slider-for').slick({
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    arrows: false,
+    fade: true,
+    asNavFor: '.slider-nav'
+  });
+  $('.slider-nav').slick({
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    asNavFor: '.slider-for',
+    dots: true,
+    centerMode: true,
+    focusOnSelect: true
+  });
+}); // Полифилы
+// forEach IE 11
+
+if ('NodeList' in window && !NodeList.prototype.forEach) {
+  console.info('polyfill for IE11');
+
+  NodeList.prototype.forEach = function (callback, thisArg) {
+    thisArg = thisArg || window;
+
+    for (var i = 0; i < this.length; i++) {
+      callback.call(thisArg, this[i], i, this);
+    }
+  };
+} // closest IE 11
+
+
+(function () {
+  if (!Element.prototype.closest) {
+    Element.prototype.closest = function (css) {
+      var node = this;
+
+      while (node) {
+        if (node.matches(css)) return node;else node = node.parentElement;
+      }
+
+      return null;
+    };
+  }
+})(); // matches IE 11
+
+
+(function () {
+  if (!Element.prototype.matches) {
+    Element.prototype.matches = Element.prototype.matchesSelector || Element.prototype.webkitMatchesSelector || Element.prototype.mozMatchesSelector || Element.prototype.msMatchesSelector;
+  }
+})(); //Array.form IE 11
+
+
+if (!Array.from) {
+  Array.from = function (object) {
+    'use strict';
+
+    return [].slice.call(object);
+  };
+}
